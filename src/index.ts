7b8236bec4ee1cf677620b8cc9a6f99749ed08a5
Davide Farina <davide.farina@spindox.it>
2024-05-31 15:05:26 +0200

    update exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export {
   getAvatarTokenWithClientSecret,
 } from "./modules/auth.js";
 export { OpenAIClient } from "./modules/openai.js";
+export { Store } from "./modules/store.js";
+export { UI } from "./modules/ui.js";
+export { Tools } from "./modules/tools.js";
+export { SermasApp, SermasConfig } from "./modules/sermas.js";
