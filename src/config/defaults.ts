e5b10ebd2587e886d2a8ce79d1e7bb6d0deb4440
Giulia Costa <giulia.costa@spindox.it>
2025-10-01 16:27:58 +0200

    up

--- src/config/defaults.ts
+++ src/config/defaults.ts
@@ -1,15 +1,15 @@
 export const defaults = {
   auth: {
     PUBLIC_AUTH_URL: "http://172.17.0.1:8080/keycloak",
-    PUBLIC_AUTH_REALM: "sermas-local",
+    PUBLIC_AUTH_REALM: "vippstar-local",
     PRIVATE_AUTH_CLIENT_ID: "platform",
     PRIVATE_AUTH_CLIENT_SECRET: "platform",
     PRIVATE_API_BASE_URL: "http://api:3000/api",
   },
   sermas: {
     SERMAS_BASE_URL: "http://localhost:8080",
-    SERMAS_CLIENT_ID: "sermas-client",
+    SERMAS_CLIENT_ID: "vippstar-client",
     SERMAS_CLIENT_SECRET: "s3cr3t",
-    SERMAS_APPID: "sermas",
+    SERMAS_APPID: "vippstar",
   },
 } as const;
